1b857576714110ab4e61ece29d8f8bf91e07cdc8
paulomtemela5-coder <paulomtemela5@gmail.com>
2026-07-06 11:24:14 +0300

    Add functionality to start button for navigation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+const welcome=document.getElementById("welcome");
+const home=document.getElementById("home");
+const startBtn=document.getElementById("startBtn");
 
+startBtn.onclick=function(){
+
+welcome.style.display="none";
+
+home.style.display="block";
+
+};
