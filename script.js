a1d36b13ee3da9e4ed7a6ea878d67e271d2a9ac8
paulomtemela5-coder <paulomtemela5@gmail.com>
2026-07-06 17:40:45 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const welcome = document.getElementById("welcome");
-const dashboard = document.getElementById("dashboard");
-
-document.getElementById("startBtn").onclick = () => {
-    welcome.style.display = "none";
-    dashboard.style.display = "block";
-};
